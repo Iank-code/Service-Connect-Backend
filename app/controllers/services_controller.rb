e71ffda859c83150d15cd8adc6ed173a715a03db
barsu5489 <ebarsulai@gmail.com>
2023-06-08 13:22:53 +0300

    update and show single service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,4 +4,24 @@ class ServicesController < ApplicationController
         @services = Service.all
         render json: @services
     end
+    def show
+        @service = Service.find(params[:id])
+        render json: @service
+    end
+
+    def update 
+        @service = Service.find(params[:id])
+        if@service.update(service_params)
+            render json: @service
+        else
+            render json: @service.errors, status: :unprocessable_entity
+        end
+    end
+
+    private 
+
+    def service_params
+        # image? Upload?
+        params.require(:service).permit(:name, :description, :price, :service_provider_id)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 puts 'seeding 🌱'
 # Fake sample service providers
-service_provider1 = ServiceProvider.create(name:"provider One", summary:"summary one" )
-service_provider2 = ServiceProvider.create(name: "Provider Two", summary: "SUmmary two")
+service_provider1 = ServiceProvider.create(username:"provider One", summary:"summary one" )
+service_provider2 = ServiceProvider.create(username: "Provider Two", summary: "SUmmary two")
 
 # Fake sample services
 service1 = Service.create(name: "House cleaning", description: "Cleaning the entire house", price: 200, service_provider: service_provider1)
-service2 = Service.create(name: "Car cleaning", description: "Cleaning all types of vehicles", pricw: 230, service_provider: service_provider2)
+service2 = Service.create(name: "Car cleaning", description: "Cleaning all types of vehicles", price: 230, service_provider: service_provider2)
 
 # Fake sample for microservices 
 Microservice.create(service: service1, name: 'Toilet cleaning', price: 100)
@@ -14,5 +14,5 @@ Microservice.create(service: service1, name: 'Window cleaning', price: 180)
 Microservice.create(service: service1, name: 'Floor cleaning', price: 140)
 
 Microservice.create(service: service2, name: 'Interior Cleaning', price: 160)
-Microservice.create(servcie: servcice2, name: 'Exterior Cleaning')
+Microservice.create(service: service2, name: 'Exterior Cleaning')
 puts 'Done seeding 🌱'
