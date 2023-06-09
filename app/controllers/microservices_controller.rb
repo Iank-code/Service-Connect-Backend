2694574f7761539e79a81c1637c22a0689b40040
barsu5489 <ebarsulai@gmail.com>
2023-06-09 09:38:06 +0300

    microservice

--- app/controllers/microservices_controller.rb
+++ app/controllers/microservices_controller.rb
@@ -1,2 +1,33 @@
 class MicroservicesController < ApplicationController
+
+    def index
+        @microservices = Microservice.all
+      end
+      
+    def create 
+        @microservice = Microservice.new(microservice_params)
+        if @microservice.save
+            render json: @microservice, status: :created, location: @microservice
+        else
+            render json: @microservice.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @microservice.update(microservice_params)
+            render json: @microservice
+        else 
+            render json: @microservice.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        @microservice.destroy
+        head :no_content
+    end
+
+    private 
+    def microservice_params 
+        params.require(microservice).permit(:service_id, :name, :price)
+    end
 end
