770304ad0f0ba42d68ab143a0f32bbf02a26e794
ntwigamartin <ntwiga.martin@gmail.com>
2023-06-12 10:04:10 +0300

    crud completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,16 +2,37 @@ class ReviewsController < ApplicationController
 
     def index
         @reviews = Review.all
-        render json: @reviews
+        render json: @reviews, status: :ok
+    end
+
+    def show
+        @review = Review.find(params[:id])
+        render json: @review
     end
 
     def create
-        review = Review.create!(review_params)
-        if review
-            render json: review
+        @review = Review.create!(review_params)
+        if @review
+            render json: @review, status: :ok
         else
-            render json: {message: "Invalid review"}, status: :unprocessable_entity
+            render json: {error: "Invalid review"}, status: :unprocessable_entity
         end
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        if @review
+            @review.update(review_params)
+            render json: @review, status: :ok
+        else
+            render json: {error: "review not found"}, status: :not_found
+        end
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        head :no_content
     end
 
     private
