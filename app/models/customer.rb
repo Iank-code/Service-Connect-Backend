8ca9e2020a96aa5bd89be9f58a74be819f11cf5c
ianK-code <kian99564@gmail.com>
2023-06-23 22:33:28 +0300

    Password reset

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,7 @@ class Customer < ApplicationRecord
     has_secure_password
     has_many :bookings
     has_one_attached :file
+    # has_many: conversations
 
     validates :file, presence: true
     validates :email, {
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@
 Rails.application.routes.draw do
   post '/initiate_stk_push', to: 'mpesa#initiate_stk_push'
 
+  mount ActionCable.server => '/cable'
+
   # Other routes...
   resources :customers
   resources :bookings
@@ -24,42 +26,45 @@ Rails.application.routes.draw do
   resources :microservices
   resources :services
   resources :service_providers
-get '/current_user', to:'application#current_user'
-get '/mpesa/credentials', to:'mpesa#credentials'
-post '/mpesa/payments', to:'mpesa#create'
+  get '/current_user', to:'application#current_user'
+  get '/mpesa/credentials', to:'mpesa#credentials'
+  post '/mpesa/payments', to:'mpesa#create'
+
   # Admin routes
-post '/admin/signup', to: 'admin#create_admin'
-post '/admin/login', to: 'admin#login_admin'
-delete '/admin/:admin_id', to: 'admin#delete_admin'
+  post '/admin/signup', to: 'admin#create_admin'
+  post '/admin/login', to: 'admin#login_admin'
+  delete '/admin/:admin_id', to: 'admin#delete_admin'
 
-post '/admin/service', to: 'admin#add_service'
-delete '/admin/service/:service_id', to: 'admin#delete_service'
-
-
-# Customer routes
-post '/customer/register', to:'customers#register'
-
-get '/customer/:id', to: 'customers#show'
-
-post '/customer/login', to:'customers#login'
-
-delete '/customer/logout', to:'customers#logout'
-
-get '/customer/check_login_status', to:'customers#check_login_status'
+  post '/admin/service', to: 'admin#add_service'
+  delete '/admin/service/:service_id', to: 'admin#delete_service'
 
 
-# Service Provider routes
+  # Customer routes
+  post '/customer/register', to:'customers#register'
 
-post '/service_provider/register', to: 'service_providers#register'
-get '/service_provider', to: 'service_providers#index'
-get '/service_provider/:id', to: 'service_providers#show'
-delete '/service_provider/logout', to: 'service_providers#logout'
+  get '/customer/:id', to: 'customers#show'
 
-post '/service_provider/login', to: 'service_providers#login'
+  post '/customer/login', to:'customers#login'
 
-get '/service_provider/bookings', to:'service_provider#view_bookings'
+  delete '/customer/logout', to:'customers#logout'
 
-post '/login', to: 'sessions#login'
+  get '/customer/check_login_status', to:'customers#check_login_status'
+
+  patch '/update/password', to: "customers#update_password"
+
+
+  # Service Provider routes
+
+  post '/service_provider/register', to: 'service_providers#register'
+  get '/service_provider', to: 'service_providers#index'
+  get '/service_provider/:id', to: 'service_providers#show'
+  delete '/service_provider/logout', to: 'service_providers#logout'
+
+  post '/service_provider/login', to: 'service_providers#login'
+
+  get '/service_provider/bookings', to:'service_provider#view_bookings'
+
+  post '/login', to: 'sessions#login'
 
 end
 
